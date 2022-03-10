21a8efa29a3978edb6c4fbe695bf333c614a34a4
jose zambrano <joseyzambranovpe@gmail.com>
2022-03-10 17:50:59 -0500

    print json server

--- myApp.js
+++ myApp.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
+
 var absolutePath = __dirname + "/views/index.html"
+var obj = {"message": "Hello json"}
+
 app.use("/public", express.static(__dirname+"/public"))
+
 app.get("/",(rep,res)=>{
-    res.sendFile(absolutePath)
+    res.json(obj)
 })
 
 
