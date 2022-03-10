e004e665a01800a5cb5d793a43cf172ade99b1ea
jose zambrano <joseyzambranovpe@gmail.com>
2022-03-10 18:09:29 -0500

    modified json server

--- myApp.js
+++ myApp.js
@@ -2,12 +2,13 @@ var express = require('express');
 var app = express();
 
 var absolutePath = __dirname + "/views/index.html"
-var obj = {"message": "Hello json"}
 
 app.use("/public", express.static(__dirname+"/public"))
 
-app.get("/",(rep,res)=>{
-    res.json(obj)
+app.get("/json",(rep,res)=>{
+    res.json({
+        message:"Hello json"
+    })
 })
 
 
