feb26283c61c11667a8362352d0ff87f7c4792b9
jose zambrano <joseyzambranovpe@gmail.com>
2022-03-09 22:24:37 -0500

    add server static asset

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var absolutePath = __dirname + "/views/index.html"
+app.use("/public", express.static(__dirname+"/public"))
 app.get("/",(rep,res)=>{
     res.sendFile(absolutePath)
 })
