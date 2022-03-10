786dc066c2d6aa7d84363ba4c0940ad3215d5497
jose zambrano <joseyzambranovpe@gmail.com>
2022-03-09 21:49:03 -0500

    server an html file

--- myApp.js
+++ myApp.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
+var absolutePath = __dirname + "/views/index.html"
 app.get("/",(rep,res)=>{
-    res.send("Hello Express")
+    res.sendFile(absolutePath)
 })
 
 
