74bc856cc1bec3665ab620079b9ff36bb123d3f8
jose zambrano <joseyzambranovpe@gmail.com>
2022-03-11 16:41:32 -0500

    add .env file

--- myApp.js
+++ myApp.js
@@ -1,14 +1,32 @@
 var express = require('express');
-var app = express();
+//const res = require('express/lib/response');
+var app = express()
+var bGround = require("fcc-express-bground")
+require('dotenv').config()
 
-var absolutePath = __dirname + "/views/index.html"
+bGround.log("hello world")
 
-app.use("/public", express.static(__dirname+"/public"))
+app.get("/", (rep, res) => {
+    res.sendFile(__dirname + "/views/index.html")
+})
 
-app.get("/json",(rep,res)=>{
+app.use(express.static(__dirname + "/public"))
+app.use("/public", express.static(__dirname + "/public"))
+
+/*app.get("/json",(rep,res)=>{
     res.json({
-        message:"Hello json"
+        "message":"Hello json"
     })
+})*/
+
+app.get("/json", (rep, res) => {
+    var response = {"message": "Hello json"}
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+        response.message = response.message.toUpperCase()
+    }else{
+        response.message = response.message.toLowerCase()
+    }
+        res.json(response)
 })
 
 
@@ -47,4 +65,5 @@ app.get("/json",(rep,res)=>{
 
 
 
- module.exports = app;
+
+module.exports = app;
