0949290c03bb905b7214e782765573935bf4f678
jose zambrano <joseyzambranovpe@gmail.com>
2022-03-11 17:12:40 -0500

    correct config for challenge fcc .env

--- myApp.js
+++ myApp.js
@@ -1,9 +1,6 @@
-require('dotenv').config()
 var express = require('express');
-//const res = require('express/lib/response');
 var app = express()
-var bGround = require("fcc-express-bground")
-
+require('dotenv').config()
 
 app.get("/", (rep, res) => {
     res.sendFile(__dirname + "/views/index.html")
@@ -12,18 +9,10 @@ app.get("/", (rep, res) => {
 app.use(express.static(__dirname + "/public"))
 app.use("/public", express.static(__dirname + "/public"))
 
-/*app.get("/json",(rep,res)=>{
-    res.json({
-        "message":"Hello json"
-    })
-})*/
-
 app.get("/json", (rep, res) => {
     var response = {"message": "Hello json"}
     if (process.env.MESSAGE_STYLE === "uppercase") {
         response.message = response.message.toUpperCase()
-    }else {
-        response.message = response.message.toLowerCase()
     }
         res.json(response)
 })
